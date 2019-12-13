9c56d775736bd19fc8bcd13c5b75377c641cbdb0
aayush-grover <aayushgrover3@gmail.com>
2019-12-13 23:20:23 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
